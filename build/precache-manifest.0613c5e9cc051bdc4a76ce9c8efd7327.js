ddf963b3e0cf541042771db9f6f979ebdbee0129
dugal <dugal.williams726@gmail.com>
2019-08-29 21:11:34 +0100

    inital commit

---
+++ build/precache-manifest.0613c5e9cc051bdc4a76ce9c8efd7327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc43711895e1f2cb52685311e494fa67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "239b90e3acc01392791d",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5bcb0c292b3ce6da159e",
+    "url": "/static/js/2.7b5c01f2.chunk.js"
+  },
+  {
+    "revision": "239b90e3acc01392791d",
+    "url": "/static/js/main.e6076e2d.chunk.js"
+  },
+  {
+    "revision": "46f6293c3ca389710bde",
+    "url": "/static/js/runtime~main.cd912dc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
